c825bb3254db6830d3ff0bc95fa6f84d7604eab4
YuriSerhiienko <126407379+YuriSerhiienko@users.noreply.github.com>
2023-04-30 19:24:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,73 +1,87 @@
 phonebook = {}
 
 def input_error(func):
-    def wrapper(*args, **kwargs):
+    def inner(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            result = func(*args, **kwargs)
+            return result
+        except KeyError:
+            return "There is no such name"
         except ValueError:
             return "Give me name and phone please"
         except IndexError:
-            return "Please provide all required arguments"
-    return wrapper
+            return "Enter user name"
+        except TypeError:
+            return "Incorrect values"
+    return inner
+
+def greeting(_):
+    return "How can I help you?"
+
+def unknown_command():
+    return "Unknown command"
+
+def exit(_):
+    return None
 
 @input_error
-def add_contact(user_input):
-    name = user_input[1]
-    phone_number = int(user_input[2])
-    phonebook[name] = phone_number
-    return "Contact added successfully."
+def add_user(*args):
+    name, phone = args
+    phonebook[name] = int(phone)
+    return "Contact added successfully"
 
 @input_error
-def change_contact(user_input):
-    name = user_input[1]
-    phone_number = user_input[2]
-    if name in phonebook:
-        phonebook[name] = phone_number
-        return "Contact updated successfully."
-    else:
-        return "Contact not found."
+def change_phone(*args):
+    name, phone = args
+    phonebook[name] = phone
+    return "Contact updated successfully."
 
-@input_error
-def get_phone_number(user_input):
-    name = user_input[1]
-    if name in phonebook:
-        return phonebook[name]
-    else:
-        return "Contact not found."
-
-def show_all():
-    output = ""
+def show_all(_):
+    if not phonebook:
+        return "The phonebook is empty"
+    result = ''
     for name, phone_number in phonebook.items():
-        output += f"{name}: {phone_number}\n"
-    return output.rstrip()
+        result += f"{name}: {phone_number}\n"
+    return result.rstrip()
 
+@input_error
+def get_phone_number(name):
+    return phonebook[name]
 
+commands = {
+    'hello': greeting,
+    'add': add_user,
+    'change': change_phone,
+    'show all': show_all,
+    "phone": get_phone_number,
+    'exit': exit,
+    'good bye': exit,
+    'close': exit,
+}
 
 def main():
     while True:
-        user_input = input(">>> ").lower().split()
+        command, *args = input(">>> ").strip().split(' ', 1)
+        if commands.get(command):
+            handler = commands.get(command)
+            if args:
+                args = args[0].split()    
+                result = handler(*args)
+            else:
+                result = handler("")
+        elif args and commands.get(command + ' ' + args[0]):
+            command = command + ' ' + args[0]
+            args = args[1:]
+            handler = commands.get(command)      
+            result = handler(args)
+        else:
+            result = unknown_command()
 
-        if user_input[0] == "hello":
-            print("How can I help you?")
-
-        elif user_input[0] == "add":
-            print(add_contact(user_input))
-
-        elif user_input[0] == "change":
-            print(change_contact(user_input))
-            
-        elif user_input[0] == "phone":
-            print(get_phone_number(user_input))
-
-        elif user_input[0] == "show" and user_input[1] == "all":
-            print(show_all())
-
-        elif user_input[0] == "good" and user_input[1] == "bye" or user_input[0] == "close" or user_input[0] == "exit":
-            print("Good bye!")
+        if not result:
+            print('Good bye!')
             break
 
-        else:
-            print("Command not recognized.")
+        print(result)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
